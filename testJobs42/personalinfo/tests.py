1616b501b48da6ea7b7e6ea917ed0f9fb0519dc6
alexa <alexa.sandra87@gmail.com>
2013-05-27 21:10:11 +0300

    modify tests.py

--- testJobs42/personalinfo/tests.py
+++ testJobs42/personalinfo/tests.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-
+from django.core.urlresolvers import reverse
+from django.test.client import Client
 
 from django.utils import unittest
 from models import Person
 
 
-class SportsmanTestCase(unittest.TestCase):
+class PersonTestCase(unittest.TestCase):
     def setUp(self):
         self.person = Person.objects.create(
             last_name=u'Сидоров',
@@ -17,8 +18,14 @@ class SportsmanTestCase(unittest.TestCase):
             jabber = 'sidodovmail@jabber.ru',
             other_contacts = u'ну а здесь телефоны для связи и всякие разные другие контакты',
         )
+        self.client = Client()
+        
     def test__unicode__(self):
         person = u"%s %s" % (self.person.last_name,
                                 self.person.first_name)
         self.assertEqual(unicode(self.person), person)
+    def test_index(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        
 
