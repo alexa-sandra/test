568e33072b3c27da22e5a9887a345890bbc88675
alexa-sandra <alexa.sandra87@gmail.com>
2013-05-29 02:56:08 -0700

    add middleware

--- testJobs42/personalinfo/middleware.py
+++ testJobs42/personalinfo/middleware.py
@@ -1,1 +1,12 @@
-__author__ = 'alexa'
+from django.db import connection
+from models import HttpStoredQuery
+
+class QueryCountMiddleware(object):
+    def process_request(self, request):
+	req = HttpStoredQuery()
+        req.path = request.path
+        req.method = request.metod
+	req.user = request.user
+        req.save()
+        return None
+
