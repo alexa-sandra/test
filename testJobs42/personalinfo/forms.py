db870d5bde49cc0177ceabf045bcd98c5fd772a0
alexa-sandra <alexa.sandra87@gmail.com>
2013-06-03 07:25:02 -0700

    t6_added js libraries?add datepicker widget

--- testJobs42/personalinfo/forms.py
+++ testJobs42/personalinfo/forms.py
@@ -1,13 +1,37 @@
+from django.conf import settings
 from django.forms import ModelForm, Textarea
 from testJobs42.personalinfo.models import Person
+from django import forms
 
+class DatePickerWidget(forms.DateInput):
+    class Media:
+        css = {
+            'all': (settings.STATIC_URL+"css/ui-lightness/jquery-ui-1.10.3.custom.min.css",)
+        }
+        js = (
+            settings.STATIC_URL+"js/jquery-ui-1.10.3.custom.min.js",
+            settings.STATIC_URL+"js/jquery.ui.core.js",
+            settings.STATIC_URL+"js/jquery.ui.widget.js",
+            settings.STATIC_URL+"js/jquery.ui.datepicker.js",
+        )
+
+    def __init__(self, params='', attrs=None):
+        self.params = params
+        super(DatePickerWidget, self).__init__(attrs=attrs)
+
+    def render(self, name, value, attrs=None):
+        rendered = super(DatePickerWidget, self).render(name, value, attrs=attrs)
+        return rendered + mark_safe(u'''<script type="text/javascript">
+            $('#id_%s').datepicker({%s});
+            </script>'''%(name, self.params,))
 
 class PersonForm(ModelForm):
     class Meta:
         model = Person
         widgets = {
             'bio':Textarea({'cols':40, 'rows':10}),
-            'other_contacts':Textarea({'cols':40, 'rows':10})
+            'other_contacts':Textarea({'cols':40, 'rows':10}),
+            'birth_date':DatePickerWidget(params="dateFormat: 'dd.mm.yy', changeYear: true")
                 }
 
 
--- testJobs42/urls.py
+++ testJobs42/urls.py
@@ -8,11 +8,10 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-     url( r'^accounts/login/$', 'django.contrib.auth.views.login'),
-	 url( r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login' ),
-     
      url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+	 url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login' ),
      url(r'^$', views.index, name='index'),
      url(r'^edit/', views.edit, name='edit'),
      url(r'^admin/', include(admin.site.urls)),
+     url(r'^admin_tools/', include('admin_tools.urls')),
 )+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
