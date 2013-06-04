b02715586471c5505bb99c78cefca3176509728e
alexa <alexa.sandra87@gmail.com>
2013-06-04 22:24:56 +0300

    t_10:finish signals writing

--- testJobs42/personalinfo/admin.py
+++ testJobs42/personalinfo/admin.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from models import Person
+from models import *
+
 
 class PersonAdmin(admin.ModelAdmin):
     pass
+
+class HttpStoredQueryAdmin(admin.ModelAdmin):
+    pass
+
+class ModelsActionsAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Person, PersonAdmin)
+admin.site.register(HttpStoredQuery, HttpStoredQueryAdmin)
+admin.site.register(ModelsActions, ModelsActionsAdmin)
